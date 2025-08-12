a5ab48e6a20ed845fd4a501d6d8ab63249373714
Nafiz Zubaer <nafizzubaer61@gmail.com>
2025-08-12 22:39:36 +0600

    category

---
+++ src/routes/api/category.api.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const _ = express.Router();
+const categoryController = require("../../controller/category.controller");
+const { authGuard } = require("../../middleware/authGuard.middleware");
+
+_.route("/create-category").post;
