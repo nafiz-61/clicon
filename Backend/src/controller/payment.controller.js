25b06006bcca3a87f5bd7b1c8521ed176a74e8b9
Nafiz Zubaer <nafizzubaer61@gmail.com>
2025-11-19 20:45:35 +0600

    payment controller

--- Backend/src/controller/payment.controller.js
+++ Backend/src/controller/payment.controller.js
@@ -22,12 +22,29 @@ exports.successPayment = asyncHandler(async (req, res) => {
 //failed
 exports.failedPayment = asyncHandler(async (req, res) => {
   console.log(req.body);
+
+  const { tran_id, status } = req.body;
+  await orderModel.findOneAndUpdate(
+    { transactionId: tran_id },
+    {
+      paymentStatus: status == "FAILED" && "failed",
+      transactionId: tran_id,
+    }
+  );
   return res.redirect("https://dribbble.com/shots/19636477-Payment-Failed");
 });
 
 //cancel
 exports.cancelPayment = asyncHandler(async (req, res) => {
   console.log(req.body);
+  const { tran_id, status } = req.body;
+  await orderModel.findOneAndUpdate(
+    { transactionId: tran_id },
+    {
+      paymentStatus: status == "CANCELLED" && "canceled",
+      transactionId: tran_id,
+    }
+  );
   return res.redirect(
     "https://https://dribbble.com/shots/18962895-Payment-Declined-Lottie-Animation.app/t/FlJtkO7jKt3"
   );
